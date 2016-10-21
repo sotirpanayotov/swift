75c7c648b46a8842f2c46a415d97c19981a18138
sotirpanayotov <sotirpanayotov@yahoo.de>
2016-10-22 00:55:02 +0300

    fix leapYear

--- src/twoDataTypesVariablesOperatorsMemory/Task2_LeapYear.java
+++ src/twoDataTypesVariablesOperatorsMemory/Task2_LeapYear.java
@@ -3,14 +3,14 @@ package twoDataTypesVariablesOperatorsMemory;
 public class Task2_LeapYear {
     public static void main(String[] args) {
 
-        int year = 1900;
+        int year = 1992;
 
         boolean dividesBy4 = year % 4 == 0;
         boolean dividesBy100 = year % 100 == 0;
         boolean dividesBy400 = year % 400 == 0;
-        boolean dividesByBoth100And400 = dividesBy100 && dividesBy400;
 
-        boolean isLeapYear = (dividesByBoth100And400 || dividesBy4);
+        boolean isLeapYear = (dividesBy4 && !dividesBy100 && !dividesBy400 )
+                || (dividesBy4 && dividesBy100 && dividesBy400);
 
         System.out.println(isLeapYear);
     }
